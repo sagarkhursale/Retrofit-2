36bd61cbcf5a3061df8438a329ee29587911abd7
sagar khursale <sagar.khursale@webmatrixtech.com>
2018-10-03 18:04:57 +0530

    add Observable method to PlacesApi interface

--- app/src/main/java/com/sagar/retrofit2/http/PlacesApi.java
+++ app/src/main/java/com/sagar/retrofit2/http/PlacesApi.java
@@ -1,14 +1,22 @@
 package com.sagar.retrofit2.http;
 
 import com.sagar.retrofit2.http.apimodel.Places;
+
+import io.reactivex.Observable;
 import retrofit2.Call;
 import retrofit2.http.GET;
 
 
 public interface PlacesApi {
 
+
     @GET("json?location=18.498072,73.8328396&radius=2000&key=AIzaSyCRQIpvfeQHo8eN-hxRQOJe_I7r2TU-85Q&type=shopping_mall")
     Call<Places> getTopPlaces();
 
 
+    @GET("json?location=18.498072,73.8328396&radius=2000&key=AIzaSyCRQIpvfeQHo8eN-hxRQOJe_I7r2TU-85Q&type=shopping_mall")
+    Observable<Places> getTopPlacesObservables();
+
+
+    // end
 }
